2bc65246f8eb7a2b43cbdb9cbc29966238067ff7
Albert Herd <albert.herd27@gmail.com>
2018-10-07 12:48:20 +0200

    Initial Commit

---
+++ GroupFiles.ps1
@@ -0,0 +1,55 @@
+Param(
+    [Parameter(Mandatory=$true)]
+    [string]$sourcePath,
+    [Parameter(Mandatory=$true)]
+    [string]$targetPath
+)
+
+$global:fileTypeLookup = @{};
+$folderDateTimeFormat = "MM-yyyy"
+
+function Move-FilesIntoFoldersByMonthAndType{
+    param()
+
+    $files = Get-ChildItem -Recurse -File -Path $sourcePath
+    $filesProcessed = 0
+    
+    foreach($file in $files){
+        $folder = Get-DirectoryForFile $file
+        Copy-Item $file.FullName -Destination $folder
+        
+        $filesProcessed++
+        Write-Progress -Activity "Grouping files" -Status "$($filesProcessed) out of $($files.Count) grouped" -PercentComplete (($filesProcessed / $files.Count) * 100) 
+    }
+}
+function Get-DirectoryForFile{
+    param($file)
+
+    $monthYearDirLookup = Get-FilePathDictionary $file
+    $modifiedTimeMonthYearInternal = $file.LastWriteTime.ToString("MMyyyy")
+    
+    if($monthYearDirLookup.ContainsKey($modifiedTimeMonthYearInternal)){
+        return $monthYearDirLookup[$modifiedTimeMonthYearInternal]
+    }
+
+    $extensionWithoutDot = $file.Extension.Substring(1, $file.Extension.Length - 1)
+    $dateFolderFileName = $file.LastWriteTime.ToString($folderDateTimeFormat)
+    $newPath = $targetPath + "\" + $extensionWithoutDot + "\" + $dateFolderFileName
+    $path = New-Item -ItemType Directory $newPath -Force
+    
+    $monthYearDirLookup[$modifiedTimeMonthYearInternal] = $path.FullName
+    return $path.FullName
+}
+
+function Get-FilePathDictionary{
+    param($file)
+
+    if($global:fileTypeLookup.ContainsKey($file.Extension)){
+        return $global:fileTypeLookup[$file.Extension]
+    }
+
+    $global:fileTypeLookup.Add($file.Extension, @{})
+    return $global:fileTypeLookup[$file.Extension]
+}
+
+Move-FilesIntoFoldersByMonthAndType
